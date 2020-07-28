0aa6425d33ba5a0949ea147b70f8259b2d9e3a1e
Henrique Albert <ishenriquealbert@gmail.com>
2020-07-28 11:29:03 -0300

    added linkedin link

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,26 +9,6 @@ function Footer() {
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           <a
-            className="text-gray-500"
-            title="Facebook"
-            href="#"
-            target="_blank"
-            rel="noreferrer noopener external"
-          >
-            <svg
-              fill="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-6 h-6 hover-svg"
-              viewBox="0 0 24 24"
-              alt="Facebook Icon"
-            >
-              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-            </svg>
-          </a>
-
-          <a
             className="ml-3 text-gray-500"
             title="Instagram"
             href="https://www.instagram.com/engdayraliz/"
@@ -52,7 +32,7 @@ function Footer() {
           <a
             className="ml-3 text-gray-500"
             title="LinkedIn"
-            href="#"
+            href="https://www.linkedin.com/in/engdayraliz/"
             target="_blank"
             rel="noreferrer noopener external"
           >

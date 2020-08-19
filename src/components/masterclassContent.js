22f49f0757b86d60cbeb6891d8e7ac7edff9ed9e
Henrique Albert <ishenriquealbert@gmail.com>
2020-08-19 18:34:41 -0300

    changed masterclass info

--- src/components/masterclassContent.js
+++ src/components/masterclassContent.js
@@ -88,45 +88,8 @@ export default function MasterclassContent() {
                 <path d="M22 4L12 14.01l-3-3"></path>
               </svg>
               <span className="title-font font-medium">
-                Se destacar dentro da empresa
-              </span>
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-200 rounded flex p-4 h-full items-center">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="3"
-                className="text-orange-500 w-6 h-6 flex-shrink-0 mr-4"
-                viewBox="0 0 24 24"
-              >
-                <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                <path d="M22 4L12 14.01l-3-3"></path>
-              </svg>
-              <span className="title-font font-medium">
-                Comunicação assertiva
-              </span>
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-gray-200 rounded flex p-4 h-full items-center">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="3"
-                className="text-orange-500 w-6 h-6 flex-shrink-0 mr-4"
-                viewBox="0 0 24 24"
-              >
-                <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                <path d="M22 4L12 14.01l-3-3"></path>
-              </svg>
-              <span className="title-font font-medium">
-                Ser um lider e saber se impor
+                Como se Destacar em Processos Seletivos e no Trabalho Através
+                das Softs Skills
               </span>
             </div>
           </div>
